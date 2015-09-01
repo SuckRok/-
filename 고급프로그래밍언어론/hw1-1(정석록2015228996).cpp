ce2f2de1b45282df7bf86bcf5b5e81e751392b06
suckrok <ohavit@gmail.com>
2015-09-01 09:35:33 +0900

    test
    -_-

---
+++ 고급프로그래밍언어론/hw1-1(정석록2015228996).cpp
@@ -0,0 +1,15 @@
+//[COMP718] homework 01 2015228996 Á¤¼®·Ï
+
+#include <iostream>
+
+using namespace std;
+
+void main(){
+	double c, f;
+
+	cout<<"Celsius temperatures : ";
+	cin>>c;
+	f=(9*c)/5+32;
+	cout<<"Fahreheit temperatures : "<<f<<endl;
+
+}
